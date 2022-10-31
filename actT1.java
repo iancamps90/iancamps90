18c6fcff77a4786ce684e5d3b487e317a24aea42
iancamps90 <115107407+iancamps90@users.noreply.github.com>
2022-10-31 17:10:52 +0100

    Add files via upload

---
+++ actT1.java
@@ -0,0 +1,40 @@
+public class actT1 { // CREAMOS UN PAQUETE
+
+    public static void main(String[] args){ // CREAMOS UNA CLASE Y AÑADIMOS EL METODO VOID
+
+    // IMPRIME Los tipos de datos más comunes que son int, long, double, boolean, String.
+
+        // 1.NUMERICOS
+        byte num1=10;
+        System.out.println("La cena a costado : " + num1 + "€");
+        short num2=122;
+        System.out.println("El recibo de la luz ascendido a : " + num2 + "€");
+        int num3= 500;
+        System.out.println(" Me a costado arreglar el coche : " + num3 + "€");
+        long num4=1000000L;
+        System.out.println(" La casa de mis sueños cuesta : " + num4 + "€");
+
+        //2. DECIMALES
+        float num5=4.7f;
+        System.out.println("El raton vale : " + num5 + "€");
+        double num6=100.4d;
+        System.out.println("La pantalla del ordenador vale : " + num6 + "€");
+
+        //3. BOOLEANOS
+        boolean variable1 = false;
+        System.out.println("Mi coche es negro = " + variable1);
+        boolean variable2 = true;
+        System.out.println("Mi coche es rojo = " + variable2);
+
+        //4.
+        char variable3 = 'i';
+        System.out.println("Caracter de mi inicial es: " + variable3);
+        String variable4 = "Ian";
+        System.out.println("Mi nombre es = " + variable4);
+
+
+
+
+    }
+}
+
